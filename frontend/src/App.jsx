d2a9a2dac0367b2e69f25ff04d7ce909a9124e91
Mazed <raiyoz@live.com>
2024-10-21 04:01:10 +0700

    10th Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import MainWrapper from './layouts/MainWrapper'
+import PrivateRoute from './layouts/PrivateRoute'
+import Register from './views/auth/Register'
+import Login from './views/auth/Login'
+import Logout from './views/auth/Logout'
+import ForgotPassword from './views/auth/ForgotPassword'
+import CreateNewPassword from './views/auth/CreateNewPassword'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <MainWrapper>
+        <Routes>
+          <Route path="/register/" element={<Register />} />
+          <Route path="/login/" element={<Login />} />
+          <Route path="/logout/" element={<Logout />} />
+          <Route path="/forgot-password/" element={<ForgotPassword />} />
+          <Route path="/create-new-password/" element={<CreateNewPassword />} />
+        </Routes>
+      </MainWrapper>
+    </BrowserRouter>
+  )
+}
+
+export default App
